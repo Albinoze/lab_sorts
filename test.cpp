c5b41cd4e5b9f6cd51e5d4f73b0f8e4fdb8f5c74
Albinoze <albina.skoritsa@gmail.com>
2022-11-21 19:19:15 +0300

    Add test

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include "test.hpp"
+
+int main()
+{
+    try
+    {
+        Sorts_test();
+    }
+    catch(char *er)
+    {
+        std::cout << er << '\n';
+    }
+    
+    Seq_test();
+    
+}
